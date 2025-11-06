74914b1d09b16ed4ff69bd591e4ae14de8a75673
Mateus Silva <dev.mateushssilva@gmail.com>
2025-11-05 22:34:52 -0300

    Implementa escrita em arquivo .csv e passagem da linguagem por parâmetro

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
+import csv
 import sys
-from warnings import catch_warnings
+import os
 
 from pydriller import Repository, ModificationType
+
+clone_dir = './temp_repos'
 
 all_repo_results = []
 
 def get_repository(path):
-    return Repository(path, order='chronological', only_no_merge=True, only_modifications_with_file_types=['.py'])
+    print('Getting repository from ' + path)
 
-def process_repo(url):
+    if not os.path.exists(clone_dir):
+        os.makedirs(clone_dir)
+
+    return Repository(path.strip(), order='chronological', only_no_merge=True, only_modifications_with_file_types=['.py'], clone_repo_to=clone_dir)
+
+def process_repo(url, language):
+    print('Processing ' + url)
     repo = get_repository(url)
 
-    return {'repo': url, 'modifications': process_commits(repo)}
+    return {'repo': url, 'modifications': process_commits(repo, language)}
 
-def process_commits(repo):
+def process_commits(repo, language):
+    print('Processing commits')
     modifications = {}
     for commit in repo.traverse_commits():
         for file in commit.modified_files:
@@ -21,7 +31,7 @@ def process_commits(repo):
             old_path = file.old_path
             new_path = file.new_path
 
-            if not is_test_file(file.filename):
+            if not is_test_file(file.filename, language):
                 continue
 
             if file.change_type == ModificationType.ADD:
@@ -45,7 +55,7 @@ def process_commits(repo):
 
     return modifications
 
-def is_test_file(file):
+def is_py_test_file(file):
     if not file or not file.endswith('.py'):
         return False
 
@@ -57,25 +67,78 @@ def is_test_file(file):
 
     return False
 
+def is_js_test_file(file):
+    if not file or not file.endswith('.js'):
+        return False
+
+    if file.endswith('.test.js') or file.endswith('.spec.js'):
+        return True
+
+    return False
+
+def is_ts_test_file(file):
+    if not file or not file.endswith('.ts'):
+        return False
+
+    if file.endswith('.test.ts') or file.endswith('.spec.ts'):
+        return True
+
+    return False
+
+def is_test_file(file, language):
+    if language == 'py':
+        return is_py_test_file(file)
+    elif language == 'ts':
+        return is_ts_test_file(file)
+    elif language == 'js':
+        return is_js_test_file(file)
+    else:
+        return False
+
+def write_to_csv(results):
+    print('Writing to csv')
+    fieldnames = ['repo_url', 'file', 'modifications']
+    if not results:
+        return
+
+    with open('result.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for result in results:
+            repo_url = result['repo']
+            modifications = result['modifications']
+
+            for filepath, modifications_num in modifications.items():
+                writer.writerow({'repo_url': repo_url, 'file': filepath, 'modifications': modifications_num})
+
+    return
+
 def main():
-    if len(sys.argv) < 2:
-        print('Uso: python main.py <caminho_para_o_arquivo_de_repositorios>')
+    if len(sys.argv) < 3:
+        print('Uso: python main.py <linguagem> <caminho_para_o_arquivo_de_repositorios>')
         sys.exit(1)
 
-    file_path = sys.argv[1]
+    language = sys.argv[1]
+    file_path = sys.argv[2]
+
+    if language not in ['py', 'ts', 'js']:
+        print('As linguagens suportadas atualmente são: "py", "ts", "js"')
+        sys.exit(1)
 
     try:
         with open(file_path, 'r') as file:
             for repo_url in file:
                 try:
-                    result = process_repo(repo_url)
+                    result = process_repo(repo_url, language)
 
                     if result:
                         all_repo_results.append(result)
                 except Exception as e:
                     print(f"!!! Falha ao processar {repo_url}: {e}")
 
-        print(sorted(all_repo_results, key=lambda repo: repo['modifications'], reverse=True))
+        write_to_csv(all_repo_results)
     except FileNotFoundError:
         print(f"Erro: Arquivo {file_path} não encontrado")
         sys.exit(1)
